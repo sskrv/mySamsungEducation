019a0d67587c0fd46e95ff7192d8a3e15a517760
sskrv <135308924+sskrv@users.noreply.github.com>
2023-11-05 16:40:57 +0300

    something

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Give me 1000$ pls");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        System.out.println("Введите элементы массива (для завершения ввода введите нечисловой символ):");
+
+        while (scanner.hasNextInt()) {
+            int num = scanner.nextInt();
+            list.add(num);
+        }
+
+        // Преобразование ArrayList в массив
+        Integer[] array = list.toArray(new Integer[0]);
+
+        System.out.println("Введенный массив: " + java.util.Arrays.toString(array));
     }
 }
